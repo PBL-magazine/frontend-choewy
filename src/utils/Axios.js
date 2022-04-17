5852f94f659bd88988271a55666926e7f694eb73
choewy <choewy32@gmail.com>
2022-04-17 21:11:21 +0900

    주석

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -4,12 +4,14 @@ import { Cookies } from 'react-cookie';
 const cookies = new Cookies();
 
 const Axios = () => {
+  /* 쿠키에서 토큰 정보를 가져옵니다. */
   const token = cookies.get('token');
 
+  /* 토큰 정보를 요청 Header에 추가합니다. */
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    console.log(axios.defaults.headers.common);
   }
+
   return axios;
 };
 
