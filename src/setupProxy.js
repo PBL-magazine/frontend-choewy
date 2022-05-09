cc7a6d12f007ce3b657951310cc68604a7462dba
Steve Park <56494905+diasm3@users.noreply.github.com>
2022-05-09 09:07:44 +0700

    자꾸 접속 시도가 들어와서 서버 아이피 삭제합니다.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     ['/api','/uploads'], //자신이 설정한 업로드 API이름을 입력한다
     createProxyMiddleware({
-      target: 'http://52.221.217.61', // target host API 서버 입력
+      target: 'http://localhost:3000', // target host API 서버 입력
       changeOrigin: true,
     })
   );
